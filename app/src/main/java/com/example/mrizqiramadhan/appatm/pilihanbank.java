b1a5aa484de5a89217d1cf90a659d38cf20ec971
mrisqir <earlrizqi@gmail.com>
2017-03-20 13:58:26 +0700

    first commit

---
+++ app/src/main/java/com/example/mrizqiramadhan/appatm/pilihanbank.java
@@ -0,0 +1,63 @@
+package com.example.mrizqiramadhan.appatm;
+
+/**
+ * Created by MRizqi Ramadhan on 20/02/2017.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class pilihanbank extends AppCompatActivity {
+    Button btnceksaldo, btntarik, btnsetor, btntransfer, btnexit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pilihanbank);
+
+        btnceksaldo = (Button) findViewById(R.id.btnceksaldo);
+        btnsetor = (Button) findViewById(R.id.btnsetor);
+        btntarik = (Button) findViewById(R.id.btntarik);
+        btntransfer = (Button) findViewById(R.id.btntransfer);
+        btnexit = (Button) findViewById(R.id.btnexit);
+
+        btntarik.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(pilihanbank.this, penarikan.class);
+                startActivity(i);
+            }
+        });
+        btnsetor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(pilihanbank.this, penyetoran.class);
+                startActivity(i);
+            }
+        });
+        btnceksaldo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(pilihanbank.this, cek.class);
+                startActivity(i);
+            }
+        });
+        btntransfer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(pilihanbank.this, transfer.class);
+                startActivity(i);
+            }
+        });
+        btnexit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                System.exit(0);
+            }
+        });
+    }
+}
